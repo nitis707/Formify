b14a4b78fb788ffc4bea2ac6270205ea0cb8ff02
Nitish Singh <nitishkumarsingh909@gmail.com>
2025-03-27 23:37:36 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>I am home!</h1>
+    <div className="mt-10">
+      <h1 className="bg-red-500 text-white">I am home!</h1>
       <p>I am paragraph!</p>
       <p>I am paragraph1!</p>
     </div>
