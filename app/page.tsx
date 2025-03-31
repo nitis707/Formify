3d210797d75bbcd924f95c30486ac8e2c881eb71
Nitish Singh <nitishkumarsingh909@gmail.com>
2025-03-31 21:02:56 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <h1 className="bg-red-500 text-white">I am home!</h1>
       <p className="bg-red-500 text-white">I am paragraph!</p>
       <p className="bg-red-500 text-white">I am paragraph1!</p>
+      <h2>Hello</h2>
     </div>
   );
 }
